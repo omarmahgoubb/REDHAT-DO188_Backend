d91e7236294d6c12f2291b361775957441b6fbac
omarmahgoubb <150741940+omarmahgoubb@users.noreply.github.com>
2024-12-21 01:25:38 +0200

    Update app.py

--- app.py
+++ app.py
@@ -10,7 +10,8 @@ from bson import ObjectId
 
 app = Flask(__name__)
 #CORS(app, supports_credentials=True, origins=["http://localhost:4200"])
-CORS(app, supports_credentials=True, origins=["*"])
+#CORS(app, supports_credentials=True, origins=["*"])
+CORS(app, supports_credentials=True, origins=["https://my-angular-app-omarmahgoub-dev.apps.rm3.7wse.p1.openshiftapps.com"])
 #app.config["MONGO_URI"] = "mongodb://localhost:27017/package_tracking_system"
 app.config["MONGO_URI"] = "mongodb://mymongo:27017/package_tracking_system"
 
